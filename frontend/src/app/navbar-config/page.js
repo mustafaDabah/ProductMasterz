10068d4f5825aa6eedf6ec9b1789e41770ff994e
mustafaDabah <mustafadabah5555@gmail.com>
2023-09-19 14:49:32 +0300

    fix navbar tab select box in create page

--- frontend/src/app/navbar-config/page.js
+++ frontend/src/app/navbar-config/page.js
@@ -1,16 +1,39 @@
 import NavbarConfig from '@/Components/NavbarConfig/NavbarConfig';
-import { Title } from '@/Components/UI';
+import { CurrentUser, Title } from '@/Components/UI';
+import { authOptions } from '@/utils/authOptions';
+import { getData } from '@/utils/basicURL';
+import { getServerSession } from 'next-auth';
 import React from 'react'
+export const metadata = {
+  title: "Navbar Config page",
+};
 
-function page() {
+const fetchData = async () => {
+  const adminMails = await getData("admin-mails");
+
+  return adminMails
+}
+
+async function page() {
+  const adminMails = await fetchData();
+  const session = await getServerSession(authOptions);
+
+  const isAdmin = adminMails.find(
+    (mail) => mail.email === session?.user?.email
+  );
+
+  if (!isAdmin) {
+    redirect("/api/auth/signin");
+  }
   return (
     <section className='pt-3'>
-        <div className='container'>
-           <Title text='Navbar Config Page' />
-           <NavbarConfig />
-        </div>
+      <div className='container'>
+        <CurrentUser />
+        <Title text='Navbar Config Page' />
+        <NavbarConfig />
+      </div>
     </section>
-    
+
   )
 }
 
