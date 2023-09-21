6b370629912fd8f5767fc7a2d4204ddae946a05b
Mustafa Dabah <65419936+mustafaDabah@users.noreply.github.com>
2023-09-21 13:27:10 +0300

    remove Calendly popup

--- frontend/src/Components/Sections/About/Components/Popup/Popup.jsx
+++ frontend/src/Components/Sections/About/Components/Popup/Popup.jsx
@@ -1,32 +1,9 @@
-import { Calendly } from '@/Components/UI'
-import useClickOutsideAndEscape from '@/Hooks/useClickOutsideAndEscape';
 
 function Popup({ show, closePopup, infoData }) {
-    const { calendlyMeeting } = infoData[0].fields; 
-    const popupRef = useClickOutsideAndEscape(closePopup);
 
     return (
         <div className={`overlay-module ${show ? 'show' : ''}`}>
-            <div
-                className={`modal fade game-modal ${show ? 'show fadeIn' : ''}`}
-                id="App6"
-            >
-                <div className="modal-dialog modal-lg" role="document">
-                    <div className="modal-content h-100 overflow-hidden" ref={popupRef}>
-                        <div className="modal-header">
-
-                            <button
-                                type="button"
-                                className="close"
-                                onClick={closePopup}
-                            >
-                                <span aria-hidden="true">×</span>
-                            </button>
-                        </div>
-                        <Calendly url={calendlyMeeting} />
-                    </div>
-                </div>
-            </div>
+           
         </div>
     )
 }
